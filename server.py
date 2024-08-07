981e0e8aa912e92aa3a93d26cc7c0b318a3236f0
WindClan <monstertoast2@gmail.com>
2024-08-07 16:01:57 -0400

    sanitize URLs

--- server.py
+++ server.py
@@ -1,15 +1,34 @@
 import socketserver
 import threading
+import re
 import os
 
 cacheFiles = not os.path.exists(".DISABLECACHE")
 pageCache = {}
+errorPageCache = {}
 
+def sanitizeUrl(dirtyUrl):
+    dirtyUrl = re.sub("[^A-Za-z0-9\/\.\-_]+","",dirtyUrl.split("?")[0])
+    newUrl = ""
+    parts = re.findall("[^/]+",dirtyUrl)
+    result = []
+    for part in parts:
+        if part == ".." and len(result) > 0:
+           result[len(result)-1] = None
+        elif part != ".." and part != ".":
+             result.append(part)
+    for part in result:
+        newUrl = newUrl+"/"+part
+    if newUrl == "":
+        newUrl = "/index.html"
+    return newUrl
 def createDir(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 def getErrorPage(code,exception=""):
-    if os.path.exists("weberrors/"+code+".html"):
+    if cacheFiles and code in errorPageCache:
+        return errorPageCache[code]
+    elif os.path.exists("weberrors/"+code+".html"):
         file = open("weberrors/"+code+".html","rb")
         content = file.read()
         file.close()
@@ -24,13 +43,13 @@ class LegacyWebServer(socketserver.BaseRequestHandler):
             req = request.split()
             if len(req) > 0:
                 if req[0] == b"GET":
+                    path = "/"
                     try:
-                        path = "/"
                         if len(req) > 1:
-                            path = req[1].decode()
+                            path = req[1].decode('cp1252')
                         if path == "/" or path[-1] == "/":
                             path = path+"index.html"
-                        path.replace("..","")
+                        path = sanitizeUrl(path)
                         if cacheFiles and path in pageCache :
                             self.request.send(pageCache[path])
                             print(str(ip)+" GET "+path+" - cached")
@@ -45,7 +64,8 @@ class LegacyWebServer(socketserver.BaseRequestHandler):
                         else:
                             self.request.send(getErrorPage("notfound"))
                             print(str(ip)+" GET "+path+" - Not found")
-                    except:
+                    except Exception as e:
+                        print(e)
                         print(str(ip)+" GET "+path+" - Internal server error")
                         self.request.send(getErrorPage("servererror"))
                 else:
@@ -54,6 +74,13 @@ class LegacyWebServer(socketserver.BaseRequestHandler):
         self.request.close()
 createDir("webroot")
 createDir("weberrors")
+if cacheFiles:
+    for fileName in os.listdir("weberrors"):
+        path = "weberrors/"+fileName
+        file = open(path,"rb")
+        content = file.read()
+        file.close()
+        errorPageCache[fileName.split(".")[0]] = content
 if __name__ == "__main__":
     socketserver.ThreadingTCPServer.allow_reuse_address = True
     with socketserver.ThreadingTCPServer(("", 80), LegacyWebServer) as server:
